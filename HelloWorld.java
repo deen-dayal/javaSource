769a1893f51ab03029973c8fc05dfcabd8172e23
Deendayal B <deendayal.b@paconsulting.com>
2018-05-23 18:23:41 +0800

    Modified.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,9 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-class HelloWorldApp {
+class TeamNimble {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
-        System.out.println("Hello Dayal!"); // Display the string.
-        System.out.println("Hello Team!"); // Display the string.
-        System.out.println("Hello Everyone!"); // Display the string.
+        System.out.println("Hello Team Nimble!"); // Display the string.
 
         Test1();
         Test2();
