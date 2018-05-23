4e13de03d13cdb9772d6286d3a3f493d76590114
Deenadayal B <deendayal.b@paconsulting.com>
2018-05-23 16:17:06 +0800

    add files

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello Dayal!"); // Display the string.
+        System.out.println("Hello Team!"); // Display the string.
+        System.out.println("Hello Everyone!"); // Display the string.
+
+        Test1();
+        Test2();
+    }
+
+    public static void Test1() {
+        System.out.println("-----------------------------------------------"); // Display the string.
+        System.out.println("Test1 results!"); // Display the string.
+        System.out.println("-----------------------------------------------"); // Display the string.
+    }
+
+    public static void Test2() {
+        System.out.println("-----------------------------------------------"); // Display the string.
+        System.out.println("Test2 results!"); // Display the string.
+        System.out.println("-----------------------------------------------"); // Display the string.
+    }
+}
